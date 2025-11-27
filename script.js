2e11351c3a351a7ed84f540a3a44353e50821fdd
Ha Cao <xuanhavcdn@gmail.com>
2025-11-27 20:15:59 +0700

    Update image

--- script.js
+++ script.js
@@ -43,11 +43,8 @@ document.addEventListener("DOMContentLoaded", () => {
       { src: "assets/taphuanai/z6881239788441_5d2da3f342e74a3d008740233835b83c.jpg", group: "ai" },
       { src: "assets/taphuanai/z6881239788655_725dce370ff307a83eadf44e7bfbca9e.jpg", group: "ai" },
       { src: "assets/taphuanai/z6881239789169_4d9f535f5fa9bbd89ef935d47ce9206e.jpg", group: "ai" },
-      { src: "assets/taphuanai/z6881239789214_1f8b00e35f1aca3027ad80bb5a9eb725.jpg", group: "ai" },
       { src: "assets/taphuanai/z6881239789899_a7cf1c478298a2cc135fa8575c29a3ea.jpg", group: "ai" },
-      { src: "assets/taphuanai/z6881239790332_1c9e1c6663251257401b6416a0cce96e.jpg", group: "ai" },
       { src: "assets/taphuanai/z6881239790363_9f3490a61686d9ced2196a2a34a5a12f.jpg", group: "ai" },
-      { src: "assets/taphuanai/z6881239791256_38020f65107b35ff88ba5e683cdefbc7.jpg", group: "ai" },
       // Tập huấn học bạ số
       { src: "assets/taphuanhocbaso/z7030832953112_eaa3dd78e4b18854f43c80626f128c53.jpg", group: "hocbaso" },
       { src: "assets/taphuanhocbaso/z7030833064297_1abff16a11ac468b1f20999d96560b91.jpg", group: "hocbaso" },
